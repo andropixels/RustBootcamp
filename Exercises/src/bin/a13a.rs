897fd13abe53fdc90cfb824db54c42335b183570
Rahul <rahul@riodefi.com>
2022-06-02 12:02:45 +0530

    exercies 13

---
+++ Exercises/src/bin/a13a.rs
@@ -0,0 +1,10 @@
+
+
+// use a hashmap to store String as key and i32 as value 
+// use a for loop to print the data in hashmap 
+
+// try inserting elements into hashmap without reference 
+// see what compiler error you get!
+
+
+fn main() {}
