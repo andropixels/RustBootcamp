ffe8f802feb3cad7065b4b8e806c7e913c2eee28
Rahul <rahul@riodefi.com>
2022-06-02 11:39:27 +0530

    exercises1

---
+++ Exercises/src/bin/a1a.rs
@@ -0,0 +1,4 @@
+// Write function which accepts borrowed string and print it, write your first and last name using two different functions
+
+
+fn main() {}
---
+++ Exercises/src/bin/a1b.rs
@@ -0,0 +1,5 @@
+// write a function which returns a addition of two arguments 
+// write a function which returns a Multiplication of two arguments 
+// write a function which returns a division of two arguments 
+
+fn main() {}
