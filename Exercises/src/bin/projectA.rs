92d0e1e24db751980c4ed9f5e8aa006b15734358
Rahul <rahul@riodefi.com>
2022-06-02 13:43:10 +0530

    projectA

---
+++ Exercises/src/bin/projectA.rs
@@ -0,0 +1,14 @@
+// Define a enum Frontend having different frontend frameworks , react,angular, vue
+// Impl Frontend for a print method use match statement to print different frameworks 
+// Define a enum Backend  with languages like  Rust, Go, python
+// Impl Backend for a print method use match statement to print different backend languages
+// Define a struct Techstack having two fields frontend:Frontend, backend:Backend
+// Impl TechStack for a print method which calls the print method over frontend and backend
+// Define a struct Project  with two fields techstack:TechStack and deadline:i32
+// Impl Product for a associated function new which returns the Self 
+// Create a print method here also which calls the print method over the TechStack, but check if the backend language is Rust and the deadline is less than 10 then only call the print method over TechStack .
+
+
+fn main() {
+    
+}
