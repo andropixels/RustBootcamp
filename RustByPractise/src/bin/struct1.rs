c74c201a2288356f8f3c44c9198784287c158c11
Rahul <rahul@riodefi.com>
2022-06-19 00:44:51 +0530

    struct

---
+++ RustByPractise/src/bin/struct1.rs
@@ -0,0 +1,21 @@
+
+// Fill the blank and fix the error without adding/removing new line
+struct Person {
+    name: String,
+    age: u8,
+}
+fn main() {
+    let age = 18;
+    let p = Person {
+        name: String::from("sunface"),
+        age,
+    };
+
+    // How can you believe sunface is only 18? 
+    p.age = 30;
+
+    // Fill the blank
+    __ = String::from("sunfei");
+
+    println!("Success!");
+}
